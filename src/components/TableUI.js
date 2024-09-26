fd019a3af88538d88eb31edb06b8dd82a38ea63d
Vaishnavii621 <nellepalli_pv@ee.iitr.ac.in>
2024-09-26 20:12:30 +0530

    Modified TableUI.js and TableUI.css for product filter squares, design updates, and 3D effect

--- src/components/TableUI.js
+++ src/components/TableUI.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGripVertical, faEllipsisV, faTrashCan } from '@fortawesome/free-solid-svg-icons';
-import Tooltip from "@mui/material/Tooltip";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
 import IconButton from "@mui/material/IconButton";
-import EditIcon from '@mui/icons-material/Edit';
+import EditIcon from "@mui/icons-material/Edit";
+import Tooltip from "@mui/material/Tooltip";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsisVertical, faGripVertical, faTrashAlt, faEdit } from '@fortawesome/free-solid-svg-icons';
 import './TableUI.css';
-
 import image1 from "../images/01_10_discount_saree.jpg";
 import image2 from "../images/02_20_discount_one piece.jpg";
 import image3 from "../images/03_10_discount_lehenga.jpg";
@@ -25,15 +26,30 @@ import image15 from "../images/15_10_discount_kurthi.jpg";
 import image16 from "../images/16_no_discount_nightwear.jpg";
 
 const sampleImages = [
-  image1, image2, image3, image4, image5, image6, image7, image8, image9, image10,
-  image11, image12, image13, image14, image15, image16
+  image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13, image14, image15, image16
 ];
 
 const initialData = [
-  { id: "1", productFilter: "Saree", variants: [image1, image9] },
-  { id: "2", productFilter: "Lehenga", variants: [image8, image7] },
-  { id: "3", productFilter: "One piece", variants: [image2, image11] },
-  { id: "4", productFilter: "Chudidhar", variants: [image4, image5] }
+  {
+    id: "1",
+    productFilter: "Saree",
+    variants: [image1, image9],
+  },
+  {
+    id: "2",
+    productFilter: "Lehenga",
+    variants: [image8, image7],
+  },
+  {
+    id: "3",
+    productFilter: "One piece",
+    variants: [image2, image11],
+  },
+  {
+    id: "4",
+    productFilter: "Chudidhar",
+    variants: [image4, image5],
+  }
 ];
 
 const TableUI = () => {
@@ -43,9 +59,14 @@ const TableUI = () => {
   const [selectedCell, setSelectedCell] = useState(null);
   const [showDeleteColumnMenu, setShowDeleteColumnMenu] = useState(false);
   const [columnToDelete, setColumnToDelete] = useState(null);
+  const [filterText, setFilterText] = useState({}); // State to manage filter text input
 
   const addState = () => {
-    const newState = { id: `state-${tableData.length + 1}`, productFilter: "Add Filter", variants: Array(variantCount).fill(null) };
+    const newState = {
+      id: `state-${tableData.length + 1}`,
+      productFilter: "Add Filter",
+      variants: Array(variantCount).fill(null),
+    };
     setTableData([...tableData, newState]);
   };
 
@@ -68,6 +89,13 @@ const TableUI = () => {
     const updatedData = [...tableData];
     updatedData[index].productFilter = newFilter;
     setTableData(updatedData);
+  };
+
+  const handleAddFilterClick = (index) => {
+    const newText = prompt("Enter new product filter:", tableData[index].productFilter);
+    if (newText !== null) {
+      setFilterText((prev) => ({ ...prev, [index]: newText }));
+    }
   };
 
   const handleAddDesign = (rowIndex, columnIndex) => {
@@ -112,8 +140,12 @@ const TableUI = () => {
         <DragDropContext onDragEnd={handleDragEnd}>
           <Droppable droppableId="droppable">
             {(provided) => (
-              <div {...provided.droppableProps} ref={provided.innerRef} className="table-content">
-                <table className="table w-full">
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className="table-content"
+              >
+                <table className="table-auto w-full">
                   <thead>
                     <tr>
                       <th className="fixed-width">Serial No</th>
@@ -121,9 +153,9 @@ const TableUI = () => {
                       {Array.from({ length: variantCount }, (_, i) => (
                         <th key={i} className="variant-columns-header">
                           Variant {i + 1}
-                          <Tooltip title="Options" arrow>
-                            <FontAwesomeIcon icon={faEllipsisV} className="delete-column-icon" onClick={() => openDeleteColumnMenu(i)} />
-                          </Tooltip>
+                          <IconButton onClick={() => openDeleteColumnMenu(i)} className="delete-column-icon">
+                            <FontAwesomeIcon icon={faEllipsisVertical} />
+                          </IconButton>
                         </th>
                       ))}
                       <th className="fixed-width">Add Column</th>
@@ -143,46 +175,50 @@ const TableUI = () => {
                                 <FontAwesomeIcon icon={faGripVertical} />
                               </div>
                               {index + 1}
-                              <div className="row-hover-actions">
-                                <Tooltip title="Delete Row" arrow>
-                                  <FontAwesomeIcon icon={faTrashCan} className="delete-row" onClick={() => removeState(index)} />
-                                </Tooltip>
-                              </div>
+                              <Tooltip title="Delete Row" arrow>
+                                <IconButton
+                                  color="secondary"
+                                  onClick={() => removeState(index)}
+                                  className="delete-row"
+                                >
+                                  <FontAwesomeIcon icon={faTrashAlt} style={{ color: 'red' }} className="text-red-500" />
+                                </IconButton>
+                              </Tooltip>
                             </td>
                             <td className="fixed-width">
-                              <input
-                                type="text"
-                                value={state.productFilter}
-                                onChange={(e) => handleFilterChange(index, e.target.value)}
-                                className="filter-input"
-                              />
+                              <div className="filter-box" onClick={() => handleAddFilterClick(index)}>
+                                {filterText[index] || state.productFilter}
+                              </div>
                             </td>
                             {state.variants.map((variant, i) => (
                               <td key={i} className="variant-cell">
                                 {variant ? (
-                                  <div className="variant-container">
-                                    <img src={variant} alt={`Design ${i + 1}`} className="variant-img" />
-                                    <div className="edit-hover-actions">
-                                      <Tooltip title="Edit Design" arrow>
-                                        <IconButton onClick={() => handleAddDesign(index, i)} className="edit-icon">
-                                          <EditIcon />
-                                        </IconButton>
-                                      </Tooltip>
-                                    </div>
-                                  </div>
+                                  <>
+                                    <img
+                                      src={variant}
+                                      alt={`Design ${i + 1}`}
+                                      className="variant-img"
+                                    />
+                                    <Tooltip title="Edit Design" arrow>
+                                      <IconButton onClick={() => handleAddDesign(index, i)} className="edit-icon">
+                                        <FontAwesomeIcon icon={faEdit} />
+                                      </IconButton>
+                                    </Tooltip>
+                                  </>
                                 ) : (
-                                  <button onClick={() => handleAddDesign(index, i)} className="add-design-btn">
-                                    +
+                                  <button
+                                    onClick={() => handleAddDesign(index, i)}
+                                    className="add-design-btn"
+                                  >
+                                    Add Design
                                   </button>
                                 )}
                               </td>
                             ))}
                             <td className="add-column-cell">
-                              <Tooltip title="Add Column" arrow>
-                                <IconButton onClick={addVariantToAllRows} className="add-column-icon">
-                                  +
-                                </IconButton>
-                              </Tooltip>
+                              <IconButton onClick={addVariantToAllRows} className="add-column-icon">
+                                <AddIcon />
+                              </IconButton>
                             </td>
                           </tr>
                         )}
@@ -196,16 +232,19 @@ const TableUI = () => {
           </Droppable>
         </DragDropContext>
 
+        <IconButton onClick={addState} className="add-row-btn">
+          <AddIcon />
+        </IconButton>
+
         {showImageGrid && (
           <div className="overlay">
             <div className="image-grid">
               {sampleImages.map((img, i) => (
                 <div key={i} className="image-item">
                   <img src={img} alt={`Image ${i + 1}`} />
-                  <button onClick={() => handleImageInsert(img)}>Insert</button>
+                  <button onClick={() => handleImageInsert(img)} className="insert-image-btn">Insert</button>
                 </div>
               ))}
-              <button className="close-grid" onClick={() => setShowImageGrid(false)}>Close</button>
             </div>
           </div>
         )}
@@ -213,18 +252,10 @@ const TableUI = () => {
         {showDeleteColumnMenu && (
           <div className="delete-column-menu">
             <p>Are you sure you want to delete this column?</p>
-            <button onClick={deleteColumn}>Delete</button>
-            <button onClick={() => setShowDeleteColumnMenu(false)}>Cancel</button>
+            <button onClick={deleteColumn} className="confirm-delete-btn">Yes</button>
+            <button onClick={() => setShowDeleteColumnMenu(false)} className="cancel-delete-btn">No</button>
           </div>
         )}
-
-        <div className="flex justify-center mt-5 space-x-4">
-          <Tooltip title="Add Row" arrow>
-            <button onClick={addState} className="add-design-btn">
-              +
-            </button>
-          </Tooltip>
-        </div>
       </div>
     </div>
   );
